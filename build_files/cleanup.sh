7bbca39bf47eefc2464e5f9e55febbcd9fbcc920
ferret#dev <ferret.linux@gmail.com>
2026-05-18 10:31:20 +0530

    Add : /opt handeling

--- build_files/cleanup.sh
+++ build_files/cleanup.sh
@@ -2,9 +2,15 @@
 
 set -ouex pipefail
 
-### Cleanup
+### DNF Cleanup
 dnf config-manager setopt ferret-pkgs.enabled=0
 rm -f /etc/yum.repos.d/ferret-pkgs.repo
 dnf5 autoremove -y
 dnf5 clean all
 dnf5 clean packages
+
+### OSTree/bootc dir cleanup
+rm -rf /usr/etc
+rm -rf /boot && mkdir -p /boot
+find /var/* -maxdepth 0 -type d ! -name cache ! -name log -exec rm -rf {} \;
+find /var/cache/* -maxdepth 0 -type d ! -name libdnf5 -exec rm -rf {} \;
--- build_files/packages.sh
+++ build_files/packages.sh
@@ -38,7 +38,6 @@ dnf5 install -y --setopt=install_weak_deps=False \
     ghostty-zsh-completion \
     ghostty-shell-integration
 # User applications
-mkdir -p /opt/helium
 dnf5 install -y --setopt=install_weak_deps=False \
     seahorse \
     nautilus \
